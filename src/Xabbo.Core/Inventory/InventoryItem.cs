586b7610c87cb3044c4f48930eb9d67f35ffb433
QDave <davidav08@gmail.com>
2026-04-09 23:29:43 +0200

    fix unity inventory parsing

--- src/Xabbo.Core/Inventory/InventoryItem.cs
+++ src/Xabbo.Core/Inventory/InventoryItem.cs
@@ -88,9 +88,9 @@ public sealed class InventoryItem : IInventoryItem, IParserComposer<InventoryIte
         if (p.Client is ClientType.Unity)
         {
             // TODO Unity
-            // p.ReadShort(); // ?
-            // p.ReadString(); // string "r" / "s"
-            // p.ReadInt();
+            p.ReadShort(); // ?
+            p.ReadString(); // string "r" / "s"
+            p.ReadInt();
         }
 
         if (Type is ItemType.Floor)
@@ -102,8 +102,13 @@ public sealed class InventoryItem : IInventoryItem, IParserComposer<InventoryIte
             }
             else
             {
-                // TODO Unity
+                SlotId = p.ReadString();
+                Extra = p.ReadLong();
             }
+        }
+
+        if (p.Client is ClientType.Unity) {
+            p.ReadBool(); // idk what this is for, always false?
         }
     }
 
